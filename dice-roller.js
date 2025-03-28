e2dd7fcb49c65739d2420de38f2591bf1e4821cf
Julian12161 <Julian12161@gmail.com>
2025-03-28 11:43:02 +0000

    1.1

--- dice-roller.js
+++ dice-roller.js
@@ -1,4 +1,5 @@
 const readline = require("readline");
+const colors = require("@colors/colors")
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,7 +13,7 @@ function roll(min, max) {
 }
 
 const roller = function() {
-    rl.question("What dice are you rolling? ", (answer) => {
+    rl.question("What dice are you rolling? >", (answer) => {
         let parts = answer.trim().split(/d/i);
         let numDice = Number(parts[0]);
         let dieSize = Number(parts[1]);
@@ -23,7 +24,7 @@ const roller = function() {
         }
 
         const mod = function() {
-            rl.question("Is there a modifier? (enter a number or 0) ", modifier => {
+            rl.question("Is there a modifier? (enter a number or 0) >", (modifier) => {
                 let modValue = Number(modifier.trim());
 
                 if (isNaN(modValue)) {
@@ -32,12 +33,29 @@ const roller = function() {
                 }
 
                 let results = [];
-                for (let i = 0; i < numDice; i++) {
-                    results.push(roll(1, dieSize));
-                }
-                let total = results.reduce((sum, num) => sum + num, 0) + modValue;
+                let numericResults = [];
 
-                console.log(`You rolled a total of ${total}: (${results})`);
+                for (let i = 0; i < numDice; i++) {
+                    let diceRoll = roll(1, dieSize);
+                    numericResults.push(diceRoll);
+
+                    if (diceRoll === dieSize) {
+                        results.push(colors.blue(diceRoll));
+                    } else if (diceRoll === 1) {
+                        results.push(colors.red(diceRoll));
+                    } else {
+                        results.push(diceRoll);
+                    }
+                }
+                let total = numericResults.reduce((sum, num) => sum + num, 0) + modValue;
+                if (total - modifier === numDice * dieSize) {
+                    console.log(`You rolled a total of ${colors.blue.bold(total + "!")}: (${results})`);
+                } else if (total - modifier === numDice * 1) {
+                    console.log(`You rolled a total of ${colors.red(total)}: (${results})`);
+                } else {
+                    console.log(`You rolled a total of ${total}: (${results})`);
+                }
+
                 rl.close();
             })
         }
